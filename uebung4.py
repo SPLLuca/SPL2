f03c430d92873ec064083dafe3d74734a2d2c891
SPLLuca <171wita14@o365.htl-leoben.at>
2018-06-11 07:40:46 -0700

    Unterprogramm hinzufügen

--- uebung4.py
+++ uebung4.py
@@ -7,8 +7,9 @@ anzahlwuerfe = int(input("Wie oft soll simuliert werden?"))
 for i in range(0, anzahlwuerfe):
     wurf = (random.randint(1,37))
     zahlen.append(wurf)
-    print(wurf, end = hallo[random.randint(0,4)])
+    #print(wurf, end = hallo[random.randint(0,4)])
 
+print()
 
 for i in range(1,37):
     print (i,"er : ", zahlen.count(i))
